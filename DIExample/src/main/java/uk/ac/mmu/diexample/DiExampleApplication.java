cfd3ec36aff9d861a9f154de103fef6f50006868
desktop-c5hvom9\sbaker <sbaker@vermilionit.co.uk>
2025-08-22 09:29:16 +0100

    Ordering

--- DIExample/src/main/java/uk/ac/mmu/diexample/DiExampleApplication.java
+++ DIExample/src/main/java/uk/ac/mmu/diexample/DiExampleApplication.java
@@ -2,9 +2,10 @@ package uk.ac.mmu.diexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.Ordered;
 
 @SpringBootApplication
-public class DiExampleApplication implements org.springframework.boot.CommandLineRunner {
+public class DiExampleApplication implements org.springframework.boot.CommandLineRunner, Ordered {
 
     public static void main(String[] args) {
         SpringApplication.run(DiExampleApplication.class, args);
@@ -21,5 +22,10 @@ public class DiExampleApplication implements org.springframework.boot.CommandLin
     @Override
     public void run(String... args) {
         basket.chargeCreditCard("1234-5678-9012-3456", 2028, 6);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
     }
 }
