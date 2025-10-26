fc0ba476f31301e327bd2faa950c3cfbe0b0f3a7
desktop-c5hvom9\sbaker <sbaker@vermilionit.co.uk>
2025-10-26 20:46:18 +0000

    Changes to Repository example

--- ShippingCostPortsAdaptors/src/SoftwareProduct.java
+++ ShippingCostPortsAdaptors/src/SoftwareProduct.java
@@ -1,4 +1,5 @@
 import applicationcode.Provided;
+import applicationcode.Region;
 import applicationcode.Required;
 import infrastructure.driving.ShippingCostCliAdapter;
 import infrastructure.driven.ShippingCostDatabase;
@@ -10,6 +11,17 @@ public class SoftwareProduct {
 
     public static void main(String[] args) {
         ShippingCostDatabase shippingCostDatabase = new ShippingCostDatabase(); // Create an instance of ShippingCostDatabase
+        //Populate the database with some initial data
+        shippingCostDatabase.regions.add(Region.UK);
+        shippingCostDatabase.regions.add(Region.EUR);
+        shippingCostDatabase.regions.add(Region.ROW);
+        shippingCostDatabase.costPerKgMap.put(Region.UK, 0.0);
+        shippingCostDatabase.costPerKgMap.put(Region.EUR, 1.25d);
+        shippingCostDatabase.costPerKgMap.put(Region.ROW, 5.5d);
+        shippingCostDatabase.minChargeMap.put(Region.UK, 0.0);
+        shippingCostDatabase.minChargeMap.put(Region.EUR, 0.0);
+        shippingCostDatabase.minChargeMap.put(Region.ROW, 10.0);
+
         Required shippingCostDatabaseAdapter = new ShippingCostDatabaseAdapter(shippingCostDatabase); // Create an adapter for the database
         Provided shippingCostCalculator = Provided.create(shippingCostDatabaseAdapter); // Create an instance of Provided with the shipping cost database
         ShippingCostCliAdapter cli = new ShippingCostCliAdapter(shippingCostCalculator); // Create an instance of the CLI
